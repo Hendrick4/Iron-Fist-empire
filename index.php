3bde7c9baeff434cf36ba00eb4ed0ca579bbf581
Hendrick <gueret.hendrick@gmail.com>
2021-07-09 13:50:35 +0200

    Fixed connection issues

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(isset(
     )) {
         $Spseudo = mysqli_real_escape_string($db, $method['pseudo']);
         $Spwd = mysqli_real_escape_string($db, $method['pwd']);
-        $sql = "SELECT id FROM users WHERE nickname = '{$method[$Spseudo]}' AND pwd = '{$method[$pwd]}'";
+        $sql = "SELECT id,pwd FROM users WHERE nickname = '{$Spseudo}'";
         $res = $db ->query($sql);
         $user = resultAsArray($res)[0];
         $pwdCheck = password_verify($method['pwd'], $user['pwd']);
