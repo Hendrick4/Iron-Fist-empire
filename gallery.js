7d5bfe813397d034c48fbd1654af99e9ea801499
Hendrick <gueret.hendrick@gmail.com>
2021-07-13 16:09:42 +0200

    Added picture upload feature

--- gallery.js
+++ gallery.js
@@ -1,13 +1,13 @@
 $.ajax({
     url: 'gallery.php',
-    type: 'POST',
+    type: 'GET',
     data: {},
     dataType: 'json',
     success: (res, status) => {
         let images = '';
         if (res.success) {
             res.medias.forEach(media => {
-                images += "<div>" +
+                images += "<div id='div-" + media.id_media + "'>" +
                     "<h4>" + media.nom_media + "</h4>" +
                     "<img src='" + media.chemin_media + "'>" +
                     "</div>";
@@ -15,4 +15,34 @@ $.ajax({
             $("main").append(images);
         }
     }
+})
+
+$("input:submit").click((e) => {
+    e.preventDefault();
+
+    const fd = new FormData();
+    const files = $("#file")[0].files;
+
+    fd.append('file', files[0]);
+
+    $.ajax({
+        url: 'gallery.php',
+        type: 'POST',
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: (res, status) => {
+            if (res.success) {
+                $("#status").html("Upload réussie!");
+            } else {
+                $("#status").css("color", "red");
+                $("#status").html("Fichier au mauvais format. Veuillez réessayer.");
+            }
+        }
+    })
+})
+
+$("a").click((e) => {
+    e.preventDefault();
+    $("input").css("display", "block");
 })
--- gallery.php
+++ gallery.php
@@ -8,3 +8,23 @@ $sql = $db->query("SELECT * FROM medias");
 $medias = resultAsArray($sql);
 
 echo json_encode(['success'=> true, "medias"=> $medias]);
+
+if(isset($_FILES['file']['name'])) {
+    $fileName = $_FILES['file']['name'];
+
+    $location='assets/gallery/'. $fileName;
+    $imageFileType = pathinfo($location, PATHINFO_EXTENSION);
+    $imageFileType = strtolower($imageFileType);
+
+    $validExtensions = ['jpg','jpeg','gif','png'];
+
+    if(in_array($imageFileType,$validExtensions)){
+        if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+            $sql = "INSERT INTO medias(nom_media,chemin_media) VALUES ('$fileName','$location')";
+            $req = $db->query($sql);
+            echo json_encode(["success"=>true,"msg"=>'Upload reussie']);
+        };//!move uploaded file sert a uploader le fichier -> prend en parametre le chemin tmp
+    } else {
+        echo json_encode(["success"=>false,"msg"=>"Upload echouee. Veuillez réessayer."]);
+    }
+}
