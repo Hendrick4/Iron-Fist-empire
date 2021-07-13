f15511b371c5323eb7a25292a2a0c1998926631e
Hendrick <gueret.hendrick@gmail.com>
2021-07-13 15:58:40 +0200

    Added map insertion and opus insertion

--- addCharacter.js
+++ addCharacter.js
@@ -1,3 +1,29 @@
+let h = 0;
+$("#map").click((e) => {
+    e.preventDefault();
+    h++;
+    if (h <= 1) {
+        let map = "<label for='mapName'>Nom de la map :</label>" +
+            "<input type='text' id='mapName'>" +
+            "<label for='mapMedia'>Aperçu de la map :</label>" +
+            "<input type='file' id='mapMedia'>";
+        $("#mapInfo").append(map);
+        $("#createMap").css("display", "block");
+    }
+})
+
+let i = 0;
+$("#opusInfo").click((e) => {
+    e.preventDefault();
+    i++;
+    if (i <= 1) {
+        let opus = "<label>Nom de l'opus :</label>" +
+            "<input type='text' id='opusName'>";
+        $("#opus").append(opus);
+        $("#createOpus").css("display", "block");
+    }
+})
+
 $("#create").click((e) => {
     e.preventDefault();
     const fd = new FormData();
@@ -14,6 +40,7 @@ $("#create").click((e) => {
     fd.append('gender', gender);
     fd.append('age', age);
     fd.append('desc', desc);
+    fd.append('column', 'characters');
 
     $.ajax({
         url: 'addCharacter.php',
@@ -27,6 +54,57 @@ $("#create").click((e) => {
             } else {
                 $("#result").css("color", "crimson");
                 $("#result").html("Echec de la creation du personnage");
+            }
+        }
+    })
+})
+
+$("#createMap").click((e) => {
+    e.preventDefault();
+    const fd = new FormData();
+    const files = $("#mapMedia")[0].files;
+    const mapname = $("#mapName").val();
+
+    fd.append('mapName', mapname);
+    fd.append('mapMedia', files[0]);
+    fd.append('column', 'maps');
+
+    $.ajax({
+        url: 'addCharacter.php',
+        type: 'POST',
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: (res, status) => {
+            if (res.success) {
+                $("#warn").html("Creation de la map reussie!");
+            } else {
+                $("#warn").css("color", "crimson");
+                $("#warn").html("Echec de la creation de la map");
+            }
+        }
+    })
+})
+
+$("#createOpus").click((e) => {
+    e.preventDefault();
+
+    const opusName = $("#opusName").val();
+
+    $.ajax({
+        url: 'addCharacter.php',
+        type: 'POST',
+        data: {
+            column: 'opus',
+            opusName
+        },
+        dataType: 'json',
+        success: (res, success) => {
+            if (res.success) {
+                $("#warning").html("Ajout d'opus reussi!");
+            } else {
+                $("#warning").css("color", "crimson");
+                $("#warning").html("Echec de l'ajout d'opus....");
             }
         }
     })
--- addCharacter.php
+++ addCharacter.php
@@ -2,27 +2,55 @@
 
 require_once('connect.php');
 //!require_once('');
+switch($_POST['column']){
+    case 'characters':
+        if(isset(
+            $_POST['lastname'],
+            $_POST['firstname'],
+            $_POST['gender'],
+            $_POST['age'],
+            $_POST['desc'],
+            $_FILES['picture']['name']
+        )) {
+            $fileName = $_FILES['picture']['name'];
+        
+            $location='assets/gallery/'. $fileName;
+            $imageFileType = pathinfo($location, PATHINFO_EXTENSION);
+            $imageFileType = strtolower($imageFileType);
+        
+            $validExtensions = ['jpg','jpeg','gif','png'];
+            if(in_array($imageFileType,$validExtensions)){
+                if(move_uploaded_file($_FILES['picture']['tmp_name'],$location)){
+                    $sql = "INSERT INTO characters(chara_lastname,chara_firstname,chara_gender,chara_age,chara_desc,chara_picturepath) VALUES ('{$_POST['lastname']}','{$_POST['firstname']}','{$_POST['gender']}',{$_POST['age']},'{$_POST['desc']}','$location')";
+                    $req = $db->query($sql);
+                    echo json_encode(["success"=>true, "msg"=>"Succes de la creation du personnage!"]);
+                }else echo json_encode(["success"=>false]);
+            }
+        }
+        break;
+    case 'maps':
+        if(isset($_POST['mapName'],$_FILES['mapMedia']['name'])) {
+            $fileName = $_FILES['mapMedia']['name'];
+        
+            $location='assets/gallery/'. $fileName;
+            $imageFileType = pathinfo($location, PATHINFO_EXTENSION);
+            $imageFileType = strtolower($imageFileType);
+        
+            $validExtensions = ['jpg','jpeg','gif','png'];
+            if(in_array($imageFileType,$validExtensions)){
+                if(move_uploaded_file($_FILES['mapMedia']['tmp_name'],$location)){
+                    $sql = $db->query("INSERT INTO maps(map_name,map_media) VALUES ('{$_POST['mapName']}','$location')");
 
-if(isset(
-    $_POST['lastname'],
-    $_POST['firstname'],
-    $_POST['gender'],
-    $_POST['age'],
-    $_POST['desc'],
-    $_FILES['picture']['name']
-)) {
-    $fileName = $_FILES['picture']['name'];
+                    echo json_encode(["success"=>true, "msg"=>"Succes de la creation de la map!"]);
+                }else echo json_encode(["success"=>false]);
+            }
+        }
+        break;
+    case 'opus':
+        if(isset($_POST['opusName'])) {
+            $sql = $db->query("INSERT INTO opus(opus_name) VALUE ('{$_POST['opusName']}')");
 
-    $location='assets/gallery/'. $fileName;
-    $imageFileType = pathinfo($location, PATHINFO_EXTENSION);
-    $imageFileType = strtolower($imageFileType);
-
-    $validExtensions = ['jpg','jpeg','gif','png'];
-    if(in_array($imageFileType,$validExtensions)){
-        if(move_uploaded_file($_FILES['picture']['tmp_name'],$location)){
-            $sql = "INSERT INTO characters(chara_lastname,chara_firstname,chara_gender,chara_age,chara_desc,chara_picturepath) VALUES ('{$_POST['lastname']}','{$_POST['firstname']}','{$_POST['gender']}',{$_POST['age']},'{$_POST['desc']}','$location')";
-            $req = $db->query($sql);
-            echo json_encode(["success"=>true, "msg"=>"Succes de la creation du personnage!"]);
-        }else echo json_encode(["success"=>false]);
-    }
+            echo json_encode(["success"=>true, "msg"=>"Creation de l'opus reussie!"]);
+        }
+        break;
 }
